db984dd0d5c8528b1b24d05698d1a5ecc045c04f
stefanosorrenti <stefanosorrenti01@gmail.com>
2026-03-11 12:52:40 +0100

    AppHeader: add html content

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,10 +1,12 @@
+
+
 export default function AppHeader () {
 
     return (
 
         /* Header */
         <header>
-            
+            <h1>Lean Web development</h1>
         </header>
 
 
